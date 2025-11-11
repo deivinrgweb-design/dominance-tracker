2a844c73e587aa1a3f1a680bf01ccd2254fa7b9c
deivinrgweb-design <deivinrgweb@gmail.com>
2025-11-11 17:02:23 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE="dom-v2-cache";
+const CACHE="dom-v3-cache";
 const ASSETS=["/","/index.html","/manifest.webmanifest","/icons/icon-192.png","/icons/icon-512.png"];
 self.addEventListener("install", e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
 self.addEventListener("activate", e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
 self.addEventListener("fetch", e=>{
   e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{
